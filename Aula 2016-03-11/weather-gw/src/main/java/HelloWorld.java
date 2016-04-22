5d17867245c518b101b1623e175111afdff23a9a
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2016-04-22 18:17:54 +0100

    Removed directory structure for each lecture.

--- Aula 2016-03-11/weather-gw/src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lfalcao on 07/03/16.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Em primeiro lugar, boa noite. Em 2º o Sporting.");
-    }
-
-}
